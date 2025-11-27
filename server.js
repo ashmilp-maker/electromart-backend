acb4afca6273f8499997747c0ebfc22707d04d55
ashmilp-maker <ashmilp@karunya.edu.in>
2025-11-27 10:27:06 +0000

    Initial backend setup

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+let queries = []; // Temporary storage in memory
+
+app.get("/", (req, res) => {
+    res.send("API is running");
+});
+
+app.post("/queries", (req, res) => {
+    const { queryText } = req.body;
+
+    const newQuery = { 
+        id: Date.now(),
+        queryText 
+    };
+
+    queries.push(newQuery);
+
+    res.json({ success: true, data: newQuery });
+});
+
+app.get("/queries", (req, res) => {
+    res.json(queries);
+});
+
+app.listen(10000, () => console.log("🚀 Server running on port 10000"));
